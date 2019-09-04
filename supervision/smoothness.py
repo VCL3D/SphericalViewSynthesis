04fde97f4c1f4005499294695326bdff9b166b27
zuru <nzioulis@iti.gr>
2019-09-04 09:29:13 +0300

    Delete smoothness.py

--- supervision/smoothness.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def guided_smoothness_loss(input_duv, guide_duv, mask, weights):
-    guidance_weights = torch.exp(-guide_duv)
-    smoothness = input_duv * guidance_weights
-    smoothness[~mask] = 0.0
-    smoothness *= weights
-    return torch.sum(smoothness) / torch.sum(mask)
